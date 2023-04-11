38cd86bb70e379da1ade59e636f2d0020759ca1c
tharunoptimus <tharunoptimus@outlook.com>
2023-04-12 05:14:44 +0530

    Added base element initialization

--- src/elements/element.js
+++ src/elements/element.js
@@ -2,4 +2,19 @@ import Position from "../utils/position"
 import MouseEventHandler from "../utils/mouseEventHandler"
 
 export default class Element {
+	// call after entity is created in inheriting class
+	initEventHandlers() {
+		this.initHover()
+
+		// setting up MouseEventHandler
+		this.mouseEventHandle = new MouseEventHandler(this)
+
+		// dynamic listeners
+		this.domElement.addEventListener("eventListenerAdded", () =>
+			this.mouseEventHandle.resync()
+		)
+		this.domElement.addEventListener("eventListenerRemoved", () =>
+			this.mouseEventHandle.resync()
+		)
+	}
 }
